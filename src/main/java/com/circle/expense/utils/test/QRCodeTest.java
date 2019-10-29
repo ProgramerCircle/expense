6e33743e51abf0165cad4af0139ba252c2dbcf78
liuyuan <15593794849@163.com>
2019-10-29 10:45:39 +0800

    1029 commit

---
+++ src/main/java/com/circle/expense/utils/test/QRCodeTest.java
@@ -0,0 +1,52 @@
+package com.circle.expense.utils.test;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeTest {
+
+    private static final String QR_CODE_IMAGE_PATH = "D://test.png";
+
+    //生成自动跳转的二维码
+    private static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
+
+        JSONObject json = new JSONObject();
+        json.put("method", "get");
+        json.put(
+                "url",
+                text);
+        String content = json.toJSONString();// 内容
+        Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("139.196.206.168:8081", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+
+    }
+}
